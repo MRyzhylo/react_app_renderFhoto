1063033cfe59397f9618654519006290024b0f90
plyavinskiy <klerk2012@gmail.com>
2019-06-15 15:42:06 +0300

    v2.2

--- src/components/pages/Photo/index.js
+++ src/components/pages/Photo/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-
+import {GET_PHOTO} from '../../../utils/PhotoApiService';
 import './style.scss';
 
-function Photo() {
+function Photo (props) {
+   
+  const { match: {params: {photoId}}, setPhoto } = props;
+  
+  useEffect(()=>{
+    GET_PHOTO(photoId)
+    .then(photo=> {
+      setPhoto(photo)
+    });
+  },[photoId, setPhoto])
 
   return (
-    <div className="photo">
-      <h1> Hello Photo</h1>
-  
-    </div>
+  <h1>Photo Page</h1>
   );
 }
 
-export default Photo;
+const mapStateToProps = state => ({
+  photo: state.photo.data
+});
+
+const mapDispatchToProps = dispatch => ({
+  setPhoto: photo => dispatch({type: 'SET_PHOTO', photo})
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Photo);
